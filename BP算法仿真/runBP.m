5695422921d10ceacbdaedf3e6b57365078f8606
zhuzhuxia <742491842@qq.com>
2016-04-27 17:00:13 +0800

    17

--- BP算法仿真/runBP.m
+++ BP算法仿真/runBP.m
@@ -11,7 +11,7 @@ for g=0.4:0.1:0.8
     [mat,p]=CS_BP(g,img);
         subplot(2,3,k+1);
         imagesc(uint8(mat)),title('recovery');
-        Str = sprintf('(%s)  M/N=%.1f  %.3fdB',k+96,g,p);
+        Str = sprintf('(%s)  M/N=%.1f  %.3fdB',k+97,g,p);
         xlabel(Str);
     k = k+1;
 end
--- BP算法仿真/yiwei_BP.m
+++ BP算法仿真/yiwei_BP.m
@@ -1,44 +1,48 @@
+%一维信号BP重构算法
 clc
 clear all;
 close all;
-M = 200;
-N = 1000; 
-K = 30; 
+M = 500;   %观测信号长度
+N = 1000;  %稀疏信号长度
+K = 50;   %稀疏度
+%% -----1.生成稀疏度为K的稀疏信号-----
 x0 = zeros(N,1);
 p = randperm(N);
 x0(p(1:K),1) = rand(K,1)-0.5;
-
-Phi = randn(M,N);
-
-% Phia = sqrt(1/M) * Phi;
-% for i = 1:N
-%    Phia(:,i) = Phia(:,i) / norm(Phia(:,i));
-% end
-
+%------  高斯感知矩阵Phi   -------------
+Phi = sqrt(1/M) * randn(M,N);
+for i = 1:N
+    Phi(:,i) = Phi(:,i) / norm(Phi(:,i));
+end
+%-------- 测量向量 y  ----------
 y  = zeros(M,1);
 y = Phi * x0;
-ye2= mean(y.^2);
+%%  -----2. 含高斯白噪声观测 SNR=40dB --------
+ye2 = mean(y.^2);
 SNR = 40;
 sgmav = sqrt( ye2*10^(-SNR/10) );
 noisev = sgmav*randn(M,1);
 y = y + noisev;
+%% ------3.BP重构算法 ----------------
 A = [Phi,-Phi];
 b = y;
 for i =1 : length(x0)
     u(i,1) = max(0,x0(i));
     v(i,1) = max(0,-x0(i));
 end
-x1= [ u ; v];
-c= [ ones(1,length(u)) ones(1,length(v)) ];
-x1= linprog(c',[],[],A,b,zeros(size(x1)),[]);
-rec_x0 = x1(1:length(u)) - x1(length(u)+1:length(u)+length(v));
-disp('abs_erro=');
+x = [ u ; v];
+c = [ ones(1,length(u)) ones(1,length(v)) ];
+x = linprog(c',[],[],A,b,zeros(size(x)),[]);
+rec_x0 = x(1:length(u)) - x(length(u)+1:length(u)+length(v));
+%% ----4.重建质量衡量--------------
+disp('abs_err=');
 disp(norm(rec_x0 - x0) );
 disp('relative_erro=');
 disp( norm(rec_x0 - x0) / norm(x0) );
-stem(x0,'b*');
-hold on
-stem(rec_x0,'ro');
+SNR = 20 * log10( norm(x0)/norm(rec_x0-x0) )
+stem(x0,'.');
+figure;
+stem(rec_x0,'.');
 legend('原始','重构');
-% figure
-% stem(rec_x0 - x0,'r*');
+figure
+ stem(rec_x0 - x0,'.');
