1dec5f8d511bc1fd2331152c8905d6dd349dbde7
zhuzhuxia <742491842@qq.com>
2016-04-29 12:38:09 +0800

    19

---
+++ MP算法仿真/main_MP.m
@@ -0,0 +1,34 @@
+clc
+clear all;
+close all;
+M = 80;   %观测信号长度
+N = 256;  %稀疏信号长度
+K = 23;   %稀疏度
+%% -----1.生成稀疏度为K的稀疏信号-----
+x0 = zeros(N,1);
+p = randperm(N);
+x0(p(1:K),1) = randn(K,1);
+%------  高斯感知矩阵Phi   -------------
+Phi = sqrt(1/M) * randn(M,N);
+for i = 1:N
+    Phi(:,i) = Phi(:,i) / norm(Phi(:,i));
+end
+%-------- 测量向量 y  ----------
+y  = zeros(M,1);
+y = Phi * x0;
+%% -----2. MP Reconstruction ------------
+x = zeros(N,1);       %x0的逼近信号x
+times =  K;           %迭代次数 = 稀疏度
+g = zeros(N,1);       %余量和感知矩阵内积
+r  = y;               %余量初始化为y
+for n=0:times
+    g = Phi' * r;
+    [val,K] = max( abs(g) ) ;
+    x(K,1) = x(K,1) + g(K,1);
+    r = r - g(K,1) * Phi(:,K);
+end
+disp('relative error=');
+disp( norm(x-x0)/norm(x) );
+figure,  plot(x0,'b'); legend('原始');
+figure,  plot(x,'r');  legend('重构');
+figure,  plot(r);      legend('残差');
---
+++ MP算法仿真/main_MP2D.m
@@ -0,0 +1,62 @@
+clc
+close all
+clear all
+I = imread('lena128.bmp');
+[m,n] = size(I);
+N = m * n;  %稀疏信号长度
+vectorI = reshape(I,N,1);
+x0 = dct(vectorI);
+sampleRate = 0.3;  %采样率 = M/N
+M = round( N * sampleRate );
+%------  高斯感知矩阵Phi   -------------
+Phi = sqrt(1/M) * randn(M,N);
+for i = 1:N
+    Phi(:,i) = Phi(:,i) / norm(Phi(:,i));
+end
+%-------- 测量向量 y  ----------
+y  = zeros(M,1);
+y = Phi * x0;
+
+%% -----2. MP Reconstruction ------------
+x = zeros(N,1);       %x0的逼近信号x
+times =  M/2;         %迭代次数 = 稀疏度
+g = zeros(N,1);       %余量和感知矩阵内积
+r  = y;               %余量初始化为y
+for count=0:times
+    count
+    g = Phi' * r;
+    [val,K] = max( abs(g) ) ;
+    x(K,1) = x(K,1) + g(K,1);
+    r = r - g(K,1) * Phi(:,K);
+end
+idct_x = idct(x);
+rec_x = reshape(idct_x,m,n);
+%% --------3.重建质量评价--------------------
+relative_err = norm(x-x0) / norm(x)
+MSE = norm(x-x0) / (m*n)
+SNR = 10*log10( norm(x)^2 / MSE )
+Mat_rate = 1 - norm(abs(x)-abs(x0))/norm(abs(x)+abs(x0))
+figure
+subplot(121),imshow(I,[]),title('orignal image');
+subplot(122),imshow(rec_x,[]), title('reconstruct image');
+
+
+%%  ----测试数据记录--------------------------
+% times = M,sampleRate = 0.5;
+% relative_err =
+%     0.1278
+% MSE =
+%     0.2658
+% SNR =
+%    84.3585
+
+% times = M/2,sampleRate = 0.3;
+% 迭代次数为614，重建信号稀疏度K=588
+% relative_err =
+%     0.1593
+% MSE =
+%     0.3299
+% SNR =
+%    83.3882
+% Mat_rate =
+%     0.9235
