1da46e4d708b1d16ebb06efbe98a27abaa6d57c2
zhuzhuxia <742491842@qq.com>
2016-05-06 21:28:08 +0800

    29

--- 第二章相关代码/DCT.m
+++ 第二章相关代码/DCT.m
@@ -1,24 +1,14 @@
-A=imread('lena.bmp');
-C=dct2(A); %进行余弦变换
-figure;
-B=log(abs(C));
-
-figure;
-imshow(A);
-title('原始图像');
-xlabel('(a)');
-
-C(abs(C)<10)=0; %将DCT变换后的系数值小于10的元素设为0
-%E=idct2(C);
-D=idct2(C)./255; %对DCT变换值归一化，进行余弦反变换???
-figure;
-imshow(D) ;
-title('重构图像');
-xlabel('(b)');
-% imshow(uint8(E)); is the same as D=idct2(C)./255
-% imshow(E,[]); is the same as D=idct2(C)./255
-
-FF=abs(C)<10; %Compute the number of elements which are smaller than 10
-sum(sum(FF)) %result:46103
-GG=abs(C)>10; %Compute the number of elements which are larger than 10
-sum(sum(GG)) %result:19433
+function DCT()
+    img=imread('lena.bmp');
+    img_data=dct2(img);
+    figure;
+    imshow(img);
+    title('original');
+    xlabel('(a)');
+    img_data(abs(img_data)<10)=0; 
+    img_rec=idct2(img_data)./255; 
+    figure;
+    imshow(img_rec) ;
+    title('recovery');
+    xlabel('(b)');
+end
--- 第二章相关代码/one_dimensional_sampling.m
+++ 第二章相关代码/one_dimensional_sampling.m
@@ -1,12 +1,14 @@
 clc;
 clear;
-k = 1:100;
-N = 100;
-y = sin(4*k*pi/N);
-y1 = dct2(y);
-figure;
-stem(y,'.');
-title('y = sin(4*k*pi/N),k=1:100,N=100');
-figure;
-stem(y1,'.');
-title('DCT( y )');
+function ONE_DIM_SAMP()
+    k = 1:100;
+    N = 100;
+    y = sin(4*k*pi/N);
+    y1 = dct2(y);
+    figure;
+    stem(y,'.');
+    title('y = sin(4*k*pi/N),k=1:100,N=100');
+    figure;
+    stem(y1,'.');
+    title('DCT( y )');
+end
